4798724ab35666cd40f301cc4f694d035e393b8f
Hossein <mohamadhjf@gmail.com>
2024-11-29 12:58:51 +0330

    v8

---
+++ Quiz2/Servise/LoginServise.cs
@@ -0,0 +1,33 @@
+﻿using Quiz2.Continer;
+using Quiz2.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz2.Servise
+{
+    public class LoginServise
+    {
+        private readonly IUserLogin userLogin;
+        public LoginServise()
+        {
+            userLogin = new UserLogin();
+        }
+        public bool Login(string cardNumber, string password) 
+        {
+            var login = userLogin.Login(cardNumber, password);
+            if (login == true)
+            {
+                Console.WriteLine("welcom");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("card number or password is not true");
+                return false;
+            }
+        }
+    }
+}
